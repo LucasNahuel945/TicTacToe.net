cdd3916ac00d0ebbd2407b57853d009e6dbacace
Lucas Nahuel Sandoval <lusandov@emeal.nttdata.com>
2022-01-07 14:47:23 -0300

    Modificacion de la clase GameController
    Refactorización de código

--- Business/Models/GameController.cs
+++ Business/Models/GameController.cs
@@ -1,52 +1,48 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Linq;
 
 namespace Business.Models
 {
     public class GameController
     {
         private Player _playerOne;
-        private Player _playerTwo;
-        
+        private Player _playerTwo;        
         private Player _currentPlayer;
-        public Player Winner { get; private set; }
-        public Board _mainBoard;
 
+        public Player Winner { get; private set; }
+        public Board MainBoard { get; private set; }
         public bool GameOver { get; private set; }
 
         public GameController() {
-            this._playerOne = new Player(1);
+            this._playerOne = new Player(1, true);
             this._playerTwo = new Player(2);
-            this.Winner = null;
-            this._mainBoard = new Board();
             this._currentPlayer = _playerOne;
-            this.GameOver = false;
 
-            // this.ResetGame();
+            this.Winner = null;
+            this.MainBoard = new Board();
+            this.GameOver = false;
         }
 
-
-        // revisar nombre
-        public void CheckPlay(int boxPosition)
+        public void PlayOnceTime(int boxPosition)
         {
             if (!this.GameOver)
             {
-                this.Play(boxPosition);
-                this.CheckWin();               
-                this.ChecKBoardFull();  // empate        
-                this.TogglePlayer();
+                this.PaintBoxInBoard(boxPosition);
+                this.CheckWinner();               
+                this.UpdateGameStatus();  // empate        
+                this.UpdateCurrentPlayer();
             }
         }
 
-        public void Play(int boxPosition)
+        public void PaintBoxInBoard(int boxPosition)
         {
-            this._mainBoard.PaintBox(boxPosition, _currentPlayer.Id) ;
-        
+            _currentPlayer.IsPlaying = this.MainBoard
+                .PaintBox(boxPosition, _currentPlayer.Id);
         }
 
+        private void UpdateCurrentPlayer()
+        {
+            if (!this._currentPlayer.IsPlaying) this.TogglePlayer();
+        }
         public void TogglePlayer()
         {
             this._currentPlayer = (this._currentPlayer.Equals(_playerOne))
@@ -54,22 +50,30 @@ namespace Business.Models
                 : this._playerOne;
         }
 
-        private void CheckWin() {
-
-            if(this._mainBoard.GetLines().Any(line => this.CheckLine(line)))
+        private void CheckWinner()
+        {
+            if(this.GetThereIsCoincidenceInAnyLine())
             {
                 this.Winner = this._currentPlayer;
                 this.GameOver = true;
             };
         }
 
-        private bool CheckLine(int[] line) {
+        private bool GetThereIsCoincidenceInAnyLine()
+        {
+            return this.MainBoard
+                .GetLines()
+                .Any(line => this.CheckCoincidenceInLine(line));
+        }
+
+        private bool CheckCoincidenceInLine(int[] line)
+        {
             return line.All(box => box == this._currentPlayer.Id);
         }
 
-        public void ChecKBoardFull()
+        public void UpdateGameStatus()
         {
-            this.GameOver = this._mainBoard.IsFull() ? true : this.GameOver;
+            this.GameOver = this.MainBoard.IsFull() ? true : this.GameOver;
         }
     }
 }
