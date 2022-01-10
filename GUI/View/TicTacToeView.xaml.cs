f7ea1471482135074d84619939db1444a72726ca
USERSAD\marotero <mariavictoria.otero@everis.nttdata.com>
2022-01-10 17:20:06 -0300

    Creando la Interfez

--- GUI/View/TicTacToeView.xaml.cs
+++ GUI/View/TicTacToeView.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GUI.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -19,9 +20,64 @@ namespace GUI.View
     /// </summary>
     public partial class TicTacToeView : Window
     {
+        private TicTacToeViewModel _vm;
         public TicTacToeView()
         {
-            InitializeComponent();
+            this._vm = new TicTacToeViewModel();
+        }
+
+
+        private void NumberButton_Click(object sender, RoutedEventArgs e)
+        {
+            Button btn = (Button)sender;
+            var position = Convert.ToInt32(btn.Tag);
+            if (this._vm.Game(position))
+            {
+                PaintView(btn);
+                btn.Foreground = new SolidColorBrush(Colors.Black);
+            }
+            else
+            {
+                MessageBox.Show("La Casilla ya se encuentra pintada, volve a intentar!", "Error");
+            }
+
+            if (_vm._game.GameOver)
+            {
+                var message = this._vm.MessageStatusGame();
+                if(message != "")
+                {
+                    Message(message);
+                }
+
+            }
+        }
+
+        private void PaintView(Button sender)
+        {
+            if (_vm._game.GetIdCurrentPlayer() == 2)
+            {
+                sender.Content = "X";
+
+            } else { 
+
+                sender.Content = "O";
+            }
+        }
+
+
+        public void Message(string message)
+        {
+            MessageBoxResult result = MessageBox.Show(message, "TicTacToe", MessageBoxButton.YesNo);
+            switch (result)
+            {
+                case MessageBoxResult.Yes:
+                    //hay que reiniciar
+                    break;
+                case MessageBoxResult.No:
+                    MessageBox.Show("Oh well, too bad!", "My App");
+                    break;
+
+            }
         }
     }
 }
--- GUI/ViewModels/TicTacToeViewModel.cs
+++ GUI/ViewModels/TicTacToeViewModel.cs
@@ -1,12 +1,43 @@
-﻿using System;
+﻿using Business.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace GUI.ViewModels
 {
-    internal class TicTacToeViewModel
+    public class TicTacToeViewModel
     {
+
+        public GameController _game;
+
+        public TicTacToeViewModel()
+        {
+            this._game = new GameController();
+        }
+
+        public bool Game(int position)
+        {
+            return _game.PlayOnceTime(Convert.ToInt32(position));                                   
+        }
+
+        public string MessageStatusGame()
+        {
+            string status = "";
+            if (_game.Winner != null)
+            {
+                status = "Ganador del juego : " + _game.Winner.Id;
+            }
+            else
+            {
+                status = "Empate!";
+            }
+            return status;
+        }
+
+        //TODO - metodo GameOver
+
     }
 }
